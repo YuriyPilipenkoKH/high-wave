d80b2efe96e927f0bf0336b062321695b6077647
Yuriy Pilipenko <yurik2061@gmail.com>
2026-03-04 20:17:35 +0200

    latest

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,8 +1,8 @@
 import { Server as IOServer } from "socket.io";
 
 declare global {
-  // eslint-disable-next-line no-var
-  var io: IOServer | undefined;
+
+  var io: IOServer | undefined;  
 }
 
 export {};
